5284b93800b0aa2c766cc554b1b9cabde4a33279
ahbenglcb <43876574+ahbenglcb@users.noreply.github.com>
2023-01-30 08:51:41 -0500

    Delete v0.05 directory

--- v0.05/functions/softplus.m
+++
@@ -1,6 +0,0 @@
-function [phi] = softplus(x)
-%SOFTPLUS Softplus activation function
-
-    phi = log( 1 + exp(x) );
-end
-
--- v0.05/functions/softplus_grad.m
+++
@@ -1,5 +0,0 @@
-function [phi_grad] = softplus_grad(x)
-%SOFTPLUS_GRAD Derivative of softplus activation function
-
-    phi_grad = 1 ./ ( 1 + exp(-x) );
-end
